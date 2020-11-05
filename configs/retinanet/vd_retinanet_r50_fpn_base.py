0c70ba20ba9e90e857bd34eaec7bbd9e8251f2a9
ywp <380376987@qq.com>
2020-11-05 16:23:12 +0800

    Update vd_retinanet_r50_fpn_base.py

--- configs/retinanet/vd_retinanet_r50_fpn_base.py
+++ configs/retinanet/vd_retinanet_r50_fpn_base.py
@@ -103,8 +103,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=1,
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         classes=classes,
